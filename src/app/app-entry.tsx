5d754297c56e761506f90169437428cf7c795aad
Lucas Couto <lucas3couto@gmail.com>
2023-06-05 17:16:20 -0300

    chore: add tailwind

--- src/app/app-entry.tsx
+++ src/app/app-entry.tsx
@@ -6,8 +6,16 @@ const root = document.getElementById('root') as HTMLElement
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <div>
-      <h1>Welcome to Feature Sliced Boilerplate</h1>
+    <div className='min-h-screen flex flex-1 items-center justify-center text-center p-4'>
+      <div className='max-w-lg'>
+        <h1 className='text-primary font text-4xl font-black'>Welcome to Feature Sliced Boilerplate!</h1>
+        <div className='mt-4 flex items-center text-center justify-center'>
+          →
+          <a className='underline' href='https://github.com/lcoutox/react-feature-sliced-boilerplate'>
+            Clone the repository.
+          </a>
+        </div>
+      </div>
     </div>
   </React.StrictMode>
 )
