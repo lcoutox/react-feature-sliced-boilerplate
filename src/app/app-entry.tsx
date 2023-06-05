a10129afd1c9e25a4f3e9fbda1caa924cfa177d3
Lucas Couto <lucas3couto@gmail.com>
2023-06-05 12:28:46 -0300

    chore: init

---
+++ src/app/app-entry.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '@/shared/base.css'
+const root = document.getElementById('root') as HTMLElement
+
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <div>
+      <h1>Welcome to Feature Sliced Boilerplate</h1>
+    </div>
+  </React.StrictMode>
+)
