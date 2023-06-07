230480e0bb752c074affd41d3447e9858dcb434f
Lucas Couto <lucas3couto@gmail.com>
2023-06-06 22:54:06 -0300

    feat: add padding to main page

--- src/pages/signup/ui/page/page.tsx
+++ src/pages/signup/ui/page/page.tsx
@@ -2,8 +2,8 @@ import { Button, TextInput } from "@/shared/ui";
 
 export default function SignUpPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="max-w-xs w-full p-4 flex flex-col gap-8">
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <div className="md:max-w-xs w-full flex flex-col gap-8">
         <div className="flex flex-col gap-4">
           <TextInput icon="email" label="Nome" />
           <TextInput icon="email" label="Email" />
